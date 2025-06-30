5cabf652cb68d2fc57ef790ecbba3a71a098f90f
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-06-30 12:22:55 -0300

    Revert "feat(api): integrate axios and implement base API configuration"
    This reverts commit c50213e5f9f58b08a8b9d25f77c40961b957ec22.

--- src/lib/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-})
-export function setAuth(token: string, groupId?: string) {
-  api.defaults.headers.common.Authorization = `Bearer ${token}`
-  if (groupId) api.defaults.headers.common['x-group-id'] = groupId
-}
