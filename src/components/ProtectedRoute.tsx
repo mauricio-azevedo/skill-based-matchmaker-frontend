bebfb2f63e9aaf168c362decebc16c46dcc18b79
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-06-30 12:22:54 -0300

    Revert "feat(routing): implement protected routes and add login/register pages"
    This reverts commit 95eef2d6da1c85bac3a381b2fbab0aa55b673404.

--- src/components/ProtectedRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom'
-import { useAuth } from '@/context/AuthContext'
-import type { JSX } from 'react'
-
-export default function ProtectedRoute({ children }: { children: JSX.Element }) {
-  const { token } = useAuth()
-  return token ? children : <Navigate to="/login" replace />
-}
