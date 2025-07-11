4cb07c3f7cb56fb35a77c9db0a17ddc5c69fcff6
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-07-11 02:03:47 -0300

    Revert "fix(MatchesTab): update comment for clarity on `isAutoScrolling`"
    This reverts commit 83a22c82b1fbd8fe7a7cc161701a7c6a5a33c8c0.

--- src/components/MatchesTab.tsx
+++ src/components/MatchesTab.tsx
@@ -321,7 +321,7 @@ const MatchesTab: FC = () => {
           className={cn(
             'overflow-y-auto gap-12 flex flex-col',
             !isAutoScrolling && 'snap-y snap-mandatory', // só liga o snap quando NÃO estamos em auto-scroll
-            isAutoScrolling && 'pointer-events-none', // impede que o usuário arraste enquanto auto-scroll acontece
+            isAutoScrolling && 'pointer-events-none', // só liga o snap quando NÃO estamos em auto-scroll
           )}
           style={{ scrollBehavior: 'smooth' }}
         >
