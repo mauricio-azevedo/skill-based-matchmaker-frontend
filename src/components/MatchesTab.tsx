88c3f620ba53a5924e9fdbce6f45ad4b8a5808e8
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-07-11 08:18:08 -0300

    Revert "fix(MatchesTab): update snapping behavior for list layout"
    This reverts commit d72f332936bb4fe37f252f610287f6fa954fd35a.

--- src/components/MatchesTab.tsx
+++ src/components/MatchesTab.tsx
@@ -318,7 +318,7 @@ const MatchesTab: FC = () => {
         <motion.ul
           layoutScroll
           ref={listRef}
-          className={cn('overflow-y-auto gap-12 flex flex-col', 'snap-y snap-proximity')}
+          className={cn('overflow-y-auto gap-12 flex flex-col', 'snap-y snap-mandatory')}
           style={{ scrollBehavior: 'smooth' }}
         >
           <AnimatePresence initial={false}>
