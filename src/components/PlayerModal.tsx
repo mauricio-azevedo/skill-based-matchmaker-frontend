fbe522c21795cec2d3da38ae15aca9b1663cfe8d
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-07-15 13:18:59 -0300

    refactor(PlayerModal): adjust dropdown direction to improve user experience
    - Change `SelectContent` direction from `bottom` to `top`.
    - Ensure better alignment and visibility for dropdown in UI.
    
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- src/components/PlayerModal.tsx
+++ src/components/PlayerModal.tsx
@@ -200,7 +200,7 @@ const PlayerModal: FC<PlayerModalProps> = ({ mode, trigger, player }) => {
               >
                 <SelectValue placeholder="Selecione um parceiro…" />
               </SelectTrigger>
-              <SelectContent side="bottom">
+              <SelectContent side="top">
                 {selectablePlayers.map((pl) => (
                   <SelectItem
                     key={pl.id}
