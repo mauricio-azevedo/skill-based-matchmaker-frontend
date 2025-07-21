a0e8dde54d12516f59f2e299cde488245a7832d7
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-07-21 19:14:52 -0300

    refactor(PlayTab): update icon dimensions for consistency
    - Resize `PlusIcon` in "add court" button from `!w-5 !h-5` to `!w-[31px] !h-[31px]`.
    - Adjust `MoreVertical` icon dimensions in dropdown from `!w-4.5 !h-4.5` to `!w-[22px] !h-[22px]`.
    - Ensure alignment with UI standards and maintain visual harmony.
    
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- src/features/playTab/PlayTab.tsx
+++ src/features/playTab/PlayTab.tsx
@@ -84,7 +84,7 @@ export function PlayTab() {
       <div className="w-full border-b border-border min-h-11 max-h-11 flex items-center justify-center relative">
         <h2 className="text-2xl font-semibold leading-tight m-0">Quadras</h2>
         <Button variant="ghost" className="w-11 h-11 absolute right-4 bottom-0" onClick={() => addCourtWithMatch()}>
-          <PlusIcon className="!w-5 !h-5" />
+          <PlusIcon className="!w-[31px] !h-[31px]" />
         </Button>
       </div>
 
@@ -121,7 +121,7 @@ export function PlayTab() {
                       <DropdownMenu>
                         <DropdownMenuTrigger asChild>
                           <Button className="!h-11 !w-11" variant="ghost">
-                            <MoreVertical className="!w-4.5 !h-4.5" />
+                            <MoreVertical className="!w-[22px] !h-[22px]" />
                           </Button>
                         </DropdownMenuTrigger>
                         <DropdownMenuContent align="end">
