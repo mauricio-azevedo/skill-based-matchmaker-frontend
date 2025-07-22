7ad3cdc2105985d54edcc8952681251d166e304c
Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>
2025-07-22 08:08:20 -0300

    refactor(PlayTab): decrease heading size for improved hierarchy
    - Adjust heading size from `text-2xl` to `text-xl` in PlayTab header.
    
    Signed-off-by: Maurício Azevedo <mauricio.mendonca.azevedo@gmail.com>

--- src/features/playTab/PlayTab.tsx
+++ src/features/playTab/PlayTab.tsx
@@ -82,7 +82,7 @@ export function PlayTab() {
   return (
     <div className="w-full flex flex-col overflow-hidden h-full">
       <div className="w-full border-b border-border min-h-11 max-h-11 flex items-center justify-center relative">
-        <h2 className="text-2xl font-semibold leading-tight m-0">Quadras</h2>
+        <h2 className="text-xl font-semibold leading-tight m-0">Quadras</h2>
         <Button variant="ghost" className="w-11 h-11 absolute right-4 bottom-0" onClick={() => addCourtWithMatch()}>
           <PlusIcon className="!w-[24px] !h-[24px]" />
         </Button>
